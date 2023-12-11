5db28ea24ab025c140979e54f6d8ea19ee853722
Rodrigo Oliveira <projetosrodrigojava@gmail.com>
2023-12-11 17:40:25 -0300

    Criado o metodo post porém com erros

--- Back-End/aplicacao/src/main/java/br/com/api/aplicacao/controller/Controller.java
+++ Back-End/aplicacao/src/main/java/br/com/api/aplicacao/controller/Controller.java
@@ -4,18 +4,25 @@ import br.com.api.aplicacao.DAO.IUsuario;
 import br.com.api.aplicacao.model.DAO.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class Controller {
-
     @Autowired
     private IUsuario dao;
 
-    @GetMapping ("/usuarios")
+    @GetMapping("/usuarios")
     public List<Usuario> listaUsuarios () {
         return (List<Usuario>) dao.findAll();
     }
+
+
+    public Usuario criaUsuario (@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+    
 }
