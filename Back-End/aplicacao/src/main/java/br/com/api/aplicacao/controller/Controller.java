9e16eb9e71f3c5e62976229395873ef54f1072f7
Rodrigo Oliveira Santos <projetosrodrigojava@gmail.com>
2023-12-13 17:52:11 -0300

    AJustado os métodos de search,criacao,update e delete

--- Back-End/aplicacao/src/main/java/br/com/api/aplicacao/controller/Controller.java
+++ Back-End/aplicacao/src/main/java/br/com/api/aplicacao/controller/Controller.java
@@ -2,27 +2,55 @@ package br.com.api.aplicacao.controller;
 
 import br.com.api.aplicacao.DAO.IUsuario;
 import br.com.api.aplicacao.model.DAO.Usuario;
+import org.aspectj.weaver.loadtime.Options;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/usuarios")
 public class Controller {
     @Autowired
     private IUsuario dao;
 
-    @GetMapping("/usuarios")
-    public List<Usuario> listaUsuarios () {
+    @GetMapping
+    public List<Usuario> listaUsuarios() {
         return (List<Usuario>) dao.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Integer id) {
+        Optional<Usuario> optionalUsuario = dao.findById(id);
 
-    public Usuario criaUsuario (@RequestBody Usuario usuario) {
+        return optionalUsuario.map(usuario -> ResponseEntity.ok().body(usuario))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Usuario criaUsuario(@RequestBody Usuario usuario) {
         Usuario usuarioNovo = dao.save(usuario);
         return usuarioNovo;
     }
-    
+
+    @PutMapping
+    public Usuario editarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> excluirUsuario(@PathVariable Integer id) {
+        Optional<Usuario> optionalUsuario = dao.findById(id);
+
+        if (optionalUsuario.isPresent()) {
+            return ResponseEntity.ok("Usuário excluído com sucesso");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+    }
+
 }
--- Back-End/aplicacao/src/main/java/br/com/api/aplicacao/model/DAO/Usuario.java
+++ Back-End/aplicacao/src/main/java/br/com/api/aplicacao/model/DAO/Usuario.java
@@ -1,8 +1,12 @@
 package br.com.api.aplicacao.model.DAO;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 
+
+@Data
 @Entity
 @Table (name="tb_usuarios")
 public class Usuario {
@@ -21,44 +25,5 @@ public class Usuario {
     @Column(length = 200, nullable = true)
     private String telefone;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
 
